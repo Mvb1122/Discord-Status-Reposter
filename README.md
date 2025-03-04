d79ae9690f67a94f6454480c7b1b0c42257c3132
Mvb1122 <micahbushman.school@gmail.com>
2025-03-04 16:56:08 -0700

    V1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# SETUP: 
+1. Copy and fill out `BLANK_config.json`.
+2. Rename your copy to `config.json`
+3. Make sure your Discord bot has presence intent, can see the channel you specified and is in a server with specified user.
+4. Change the status of the user in question. It should send everything through.
---
+++ index.js
@@ -0,0 +1,55 @@
+const { default: AtpAgent } = require("@atproto/api");
+const Discord = require('discord.js');
+const fs = require('fs');
+
+/**
+ * @type {{ bskyName: string; bskyPass: string; discordUserID: string; discordBotToken: string; discordGuildID: string; discordChannelId: string; }} 
+ */
+const config = JSON.parse(fs.readFileSync("./config.json"));
+
+const agent = new AtpAgent({ service: 'https://bsky.social' });
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildPresences,
+    ],
+});
+
+client.on('ready', async () => {
+    await agent.login({ identifier: config.bskyName, password: config.bskyPass })
+
+    console.log("Ready!");
+})
+
+//#region Watch for Micah's status updating and save it automatically.
+let lastStatus = "";
+client.on('presenceUpdate', async (o, n) => {
+    try {
+        if (n.activities.length > 0 && (n.status != 'invisible' || n.status != 'offline') && n.userId == config.discordUserID) {
+            const thisActivity = n.activities[0];
+            const emoji = thisActivity.emoji ? thisActivity.emoji.name : "";
+            let thisStatus = (emoji + " " + thisActivity.state).trim();
+            if (thisStatus != lastStatus) {
+                lastStatus = thisStatus;
+
+                const channel = await client.channels.fetch(config.discordChannelId);
+
+                // Check that it wasn't one that I or the bot already posted.
+                /** @type {Discord.GuildMessageManager} */
+                const messages = channel.messages;
+                const alreadyPosted = (await messages.fetch({ limit: 100 })).some((v) => v.content == thisStatus)
+
+                if (!alreadyPosted) {
+                    channel.send(thisStatus);
+                    agent.post({
+                        text: thisStatus
+                    });
+                }
+            }
+        }
+    } catch {
+        // Do nothing.
+    }
+})
+
+client.login(config.discordBotToken);
