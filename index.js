b9febb12a9ea682d825e23f1983dd61c6b54e29c
Mvb1122 <micahbushman.school@gmail.com>
2025-08-16 08:57:30 -0600

    Commenting pass and renaming obtuse variable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { default: AtpAgent } = require("@atproto/api");
+const { default: AtpAgent } = require("@atproto/api"); // Bluesky AtProtocol bot stuff.
 const Discord = require('discord.js');
 const fs = require('fs');
 
 /**
  * @type {{ bskyName: string; bskyPass: string; discordUserID: string; discordBotToken: string; discordGuildID: string; discordChannelId: string; discordSuppressNotifications: boolean; }} 
  */
-const config = JSON.parse(fs.readFileSync("./config.json"));
+const config = JSON.parse(fs.readFileSync("./config.json")); // Throws error if file doesn't exist - this is intended behavior.
 
 const agent = new AtpAgent({ service: 'https://bsky.social' });
 const client = new Discord.Client({
@@ -21,12 +21,15 @@ client.on('ready', async () => {
     console.log("Ready!");
 })
 
-//#region Watch for Micah's status updating and save it automatically.
+//#region Watch for User's status updating and save it automatically.
+    // Fun fact: Because of this function being called async, there's a sad possibility that the same status can be sent multiple times.
+    // And because this is in such quick succession (I think I saw like 8 times / sec at one point), I need a variable to track the last one posted,
+    // even if we can get the same data from checking the last 100 messages... Because the message check is SLOW. :(
 let lastStatus = "";
-client.on('presenceUpdate', async (o, n) => {
+client.on('presenceUpdate', async (o, newActivity) => {
     try {
-        if (n.activities.length > 0 && (n.status != 'invisible' || n.status != 'offline') && n.userId == config.discordUserID) {
-            const thisActivity = n.activities[0];
+        if (newActivity.activities.length > 0 && (newActivity.status != 'invisible' || newActivity.status != 'offline') && newActivity.userId == config.discordUserID) {
+            const thisActivity = newActivity.activities[0];
             const emoji = thisActivity.emoji ? thisActivity.emoji.name : "";
             let thisStatus = (emoji + " " + thisActivity.state).trim();
             if (thisStatus != lastStatus) {
